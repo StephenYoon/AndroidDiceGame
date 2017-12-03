8ab3e124d60682fdbb558e467026c02aa2c42f0a
Stephen Yoon <yoonic@gmail.com>
2017-12-03 14:30:42 -0500

    Exit goes to Home Screen
    Back button on login screen goes to Home Screen

--- app/src/main/java/yoon/develop/luckydiceout/SignInActivity.java
+++ app/src/main/java/yoon/develop/luckydiceout/SignInActivity.java
@@ -142,7 +142,10 @@ public class SignInActivity extends AppCompatActivity
     @Override
     public void onBackPressed()
     {
-        // ...
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     private boolean checkFormFields() {
